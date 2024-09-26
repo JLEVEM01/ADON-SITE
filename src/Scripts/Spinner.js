cb23e4359a2565706a4ece2868336847ba628700
JLEVEM01 <jlevem23@gmail.com>
2024-09-25 18:22:54 -0600

    Commit

--- src/Scripts/Spinner.js
+++
@@ -1,9 +0,0 @@
-const container = document.getElementById('container')
-const spinner = document.getElementById('spinner')
-
-window.addEventListener('load', () =>{
-
-    container.classList.add('bg-tranparent')
-
-    spinner.classList.remove('w-28  h-28 border-8 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-red-500 rounded-full')
-})
---
+++ src/Scripts/loader.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loader  = document.getElementById('loader')
+
+    window.addEventListener('load', () => {
+        loader.classList.add('hidden', 'bg-transparent');
+    })
+})
+
